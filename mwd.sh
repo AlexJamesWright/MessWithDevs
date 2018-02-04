20b93ae6fa7808780240ca92e8bd72562e49c57f
AlexJamesWright <ajw1e16@soton.ac.uk>
2018-02-04 18:45:12 +0000

    Checks valid input file extensions

--- mwd.sh
+++ mwd.sh
@@ -1,7 +1,15 @@
-declare -a EXE=(".cc" ".c" ".cpp" ".cxx" "cu")
-REVERSE=0
+#!/bin/bash
 
-PATH_TO_PYTHON_SCRIPT="/home/alex/Documents/Play/MessWithDevs"
+declare -a EXE=(".cc" ".c" ".cpp" ".cxx" ".cu" ".c++" ".js" ".java" ".ii" \
+".ixx" ".ipp" ".i++" ".inl" ".idl" ".ddl" ".odl" ".h" ".hh" ".hxx" ".hpp" ".h++" \
+".cs" ".d" ".php" ".php4" ".php5" ".phtml" ".inc" ".m" ".md" ".mm" ".html" \
+".dox" ".py" ".pyw" ".f90" ".f95" ".f03" ".f08" ".f" ".for" ".tcl" ".vhd" ".vhdl" \
+".ucf" ".qsf")
+
+REVERSE=0
+INPUTFILE=0
+
+PATH_TO_PYTHON_SCRIPT=$(pwd)
 
 # Determine input file and direction
 for input in "$@"
@@ -12,23 +20,28 @@ do
   else
     for exe in "${EXE[@]}"
     do
-      if [ ${input: -2} == $exe ] || [ ${input: -3} == $exe ] || [ ${input: -4} == $exe ]
+      if [ ${input: -2} == $exe ] || [ ${input: -3} == $exe ] || [ ${input: -4} == $exe || [ ${input: -5} == $exe || [ ${input: -6} == $exe ]
       then
         INPUT=$input
+        INPUTFILE=1
       fi
     done
   fi
-
 done
 
-# Rename script
-cp $INPUT source.txt
-if [ $REVERSE -eq 1 ]
+if [ $INPUTFILE -eq 0 ]
 then
-  python2 $PATH_TO_PYTHON_SCRIPT/messWithDevs.py -r
+  echo 'No input file matches the valid extensions.'
 else
-  python2 $PATH_TO_PYTHON_SCRIPT/messWithDevs.py -f
-fi
+  # Rename script
+  cp $INPUT source.txt
+  if [ $REVERSE -eq 1 ]
+  then
+    python $PATH_TO_PYTHON_SCRIPT/messWithDevs.py -r
+  else
+    python $PATH_TO_PYTHON_SCRIPT/messWithDevs.py -f
+  fi
 
-mv target.txt $INPUT
-rm source.txt
+  mv target.txt $INPUT
+  rm source.txt
+fi
