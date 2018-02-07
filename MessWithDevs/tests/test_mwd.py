4fec54a9f7792e42e5864a592c5b42a0213ab0c4
AlexJamesWright <ajw1e16@soton.ac.uk>
2018-02-07 19:28:37 +0000

    Tests that all characters are reversed perfectly for both modes

--- MessWithDevs/tests/test_mwd.py
+++ MessWithDevs/tests/test_mwd.py
@@ -1,1 +1,34 @@
 import unittest
+import subprocess
+import os
+
+direc = os.path.dirname(os.path.realpath(__file__))
+
+class Test_MWD(unittest.TestCase):
+    def test_r_reverses_perfectly(self):
+        subprocess.call(['cp', 'allRelevantChars.c', 'test.c'])
+        subprocess.call(['python', '../mwd', direc + '/allRelevantChars.c'])
+        subprocess.call(['python', '../mwd', direc + '/allRelevantChars.c', '-r'])
+
+        with open('allRelevantChars.c', 'r') as converted:
+            with open('test.c', 'r') as original:
+                orig = original.readlines()
+                conv = converted.readlines()
+                self.assertEqual(len(orig), len(conv))
+                self.assertEqual(orig, conv)
+        subprocess.call(['rm', 'test.c'])
+
+    def test_rf_reverses_perfectly(self):
+        subprocess.call(['cp', 'allRelevantChars.c', 'test.c'])
+        subprocess.call(['python', '../mwd', direc + '/allRelevantChars.c'])
+        subprocess.call(['python', '../mwd', direc + '/allRelevantChars.c', '-rf'])
+
+        with open('allRelevantChars.c', 'r') as converted:
+            with open('test.c', 'r') as original:
+                orig = original.readlines()
+                conv = converted.readlines()
+                self.assertEqual(len(orig), len(conv))
+                self.assertEqual(orig, conv)
+        subprocess.call(['rm', 'test.c'])
+
+unittest.main()
