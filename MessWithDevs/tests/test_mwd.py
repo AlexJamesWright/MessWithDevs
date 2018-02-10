45e590a03e17876b0b8a0b47168576fdeb0bc754
AlexJamesWright <ajw1e16@soton.ac.uk>
2018-02-10 08:14:07 +0000

    calls the script called by commandline tool

--- MessWithDevs/tests/test_mwd.py
+++ MessWithDevs/tests/test_mwd.py
@@ -7,15 +7,16 @@ direc = os.path.dirname(os.path.realpath(__file__))
 class Test_MWD(unittest.TestCase):
     def test_r_reverses_perfectly(self):
         subprocess.call(['cp', 'allRelevantChars.c', 'test.c'])
-        subprocess.call(['python', '../mwd', direc + '/allRelevantChars.c'])
-        subprocess.call(['python', '../mwd', direc + '/allRelevantChars.c', '-r'])
+        subprocess.call(['python', '../__init__.py', direc + '/allRelevantChars.c'])
+        subprocess.call(['python', '../__init__.py', direc + '/allRelevantChars.c', '-r'])
 
         with open('allRelevantChars.c', 'r') as converted:
             with open('test.c', 'r') as original:
                 orig = original.readlines()
                 conv = converted.readlines()
                 self.assertEqual(len(orig), len(conv))
-                self.assertEqual(orig, conv)
+                for charOrig, charConv in zip(orig, conv):
+                    self.assertEqual(charOrig, charConv)
         subprocess.call(['rm', 'test.c'])
 
 
